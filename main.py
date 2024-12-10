53fef4983344f3725bac73a164c2269fa64da537
sudheer996323 <yalavarthisudheer678@gmail.com>
2024-12-10 11:41:21 -0600

    code added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion_pipeline import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -19,6 +20,17 @@ STAGE_NAME='Data Validation Stage'
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME='Data transformation Stage'
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -13,3 +13,8 @@ class DataValidationConfig:
     root_dir:Path
     STATUS_FILE:str
     all_schema:dict
+
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir:Path
+    data_path:Path
